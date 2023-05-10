f8f159886c856711aa0a098c69f35ba893594068
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-10 15:56:20 +0200

    model data created

---
+++ app/src/main/java/com/example/effectsdemo1/MainScreenState.kt
@@ -0,0 +1,7 @@
+package com.example.effectsdemo1
+
+data class MainScreenState(
+    var isCountButtonVisible : Boolean = false,
+    var displayingResult : String = "",
+    var inputValue : String = ""
+)
